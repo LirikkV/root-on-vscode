f449638523ca9cd1ba4f043804d51adb511bfcaa
Lirikk <zzxcawe.main@gmail.com>
2025-10-16 15:17:02 +0300

    Improve cycle aftr para

--- Praktikum_2025_proga/Task_1_c.C
+++ Praktikum_2025_proga/Task_1_c.C
@@ -6,18 +6,18 @@ using namespace ::std;
 
 double F_hyper(double a, double b, double c, double z)
 {
-  const double accuracy = 0.01;
-
-  double N_break_max = 5000.0;
+  const double accuracy = 1.0e-15;//DBL_EPSILON
+  const double N_break_max = 5000.0;
+  
   double N = 1.0;
 
   double X_0 = 1.0;
-  double X_multiplier = a * b * z / c;
-  double eps = fabs(X_0 * X_multiplier);
+  double eps = 1.0;
   double Sum = X_0;
-  while (eps > accuracy && eps > DBL_EPSILON && N < N_break_max)
+
+  while (eps > accuracy && N < N_break_max)
   {
-    X_multiplier = (a + N - 1.0) * (b + N - 1.0) * z / ((c + N - 1.0) * N);
+    double X_multiplier = (a + N - 1.0) * (b + N - 1.0) * z / ((c + N - 1.0) * N);
     X_0 *= X_multiplier;
     Sum += X_0;
     N += 1.0;
@@ -26,11 +26,18 @@ double F_hyper(double a, double b, double c, double z)
 
   cout << "Number of steps: " << N << endl;
   return (Sum);
+
 }
 
 void Task_1_c()
 {
+  double z = 0.99;
 
-  double x = F_hyper(0.5, 1.0, 1.5, -0.8 * 0.8);
-  printf("%e\n", x);
+  double x = F_hyper(0.5, 1.0, 1.5, -z*z);
+  printf("Hypergeometriz = %20.17f\n", x);
+  double y = (fabs(z)<DBL_EPSILON) ? 1.0 : atan(z)/z;
+  printf("Exact = %20.17f\n", y);
+  double eps = fabs((x - y)/y);
+  printf("Eps = %9.1e\n", eps);
+  //tgamma(10); rint(); pow(2,3)
 }
