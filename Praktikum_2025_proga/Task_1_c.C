330802e51f8622c12e699ec389be85db46f72c9b
Lirikk <zzxcawe.main@gmail.com>
2025-10-19 15:19:20 +0300

    Add txt alignment && |z|>1 warning

--- Praktikum_2025_proga/Task_1_c.C
+++ Praktikum_2025_proga/Task_1_c.C
@@ -6,38 +6,57 @@ using namespace ::std;
 
 double F_hyper(double a, double b, double c, double z)
 {
-  const double accuracy = 1.0e-15;//DBL_EPSILON
+  const double accuracy = 1.0e-15; // DBL_EPSILON
   const double N_break_max = 5000.0;
-  
-  double N = 1.0;
+  const double integer_accuracy = 0.001;
 
-  double X_0 = 1.0;
-  double eps = 1.0;
-  double Sum = X_0;
+  bool is_abc_not_int = fabs(rint(c - a - b) - (c - a - b)) > integer_accuracy;
 
-  while (eps > accuracy && N < N_break_max)
+  if (fabs(z) > 1)
   {
-    double X_multiplier = (a + N - 1.0) * (b + N - 1.0) * z / ((c + N - 1.0) * N);
-    X_0 *= X_multiplier;
-    Sum += X_0;
-    N += 1.0;
-    eps = fabs(X_0);
+    cout << " WARNING! Series does not converge, cause |z| > 1" << endl;
+    return (NAN);
   }
+  else if (fabs(z) <= 0.5)
+  {
+    double N = 1.0;
 
-  cout << "Number of steps: " << N << endl;
-  return (Sum);
+    double X_0 = 1.0;
+    double eps = 1.0;
+    double Sum = X_0;
 
+    while (eps > accuracy && N < N_break_max)
+    {
+      double X_multiplier = (a + N - 1.0) * (b + N - 1.0) * z / ((c + N - 1.0) * N);
+      X_0 *= X_multiplier;
+      Sum += X_0;
+      N += 1.0;
+      eps = fabs(X_0);
+    }
+
+    cout << "Number of steps: " << N << endl;
+    return (Sum);
+  }
+  // else if(0.5<z && z<=1.0 && (c) && is_abc_not_int)
+  // {
+  //   cout << "c - a - b not integer"<<endl;
+  //   return (666.666);
+  // }
 }
 
 void Task_1_c()
 {
-  double z = 0.99;
+  double z = 0.45;
 
-  double x = F_hyper(0.5, 1.0, 1.5, -z*z);
-  printf("Hypergeometriz = %20.17f\n", x);
-  double y = (fabs(z)<DBL_EPSILON) ? 1.0 : atan(z)/z;
-  printf("Exact = %20.17f\n", y);
-  double eps = fabs((x - y)/y);
-  printf("Eps = %9.1e\n", eps);
-  //tgamma(10); rint(); pow(2,3)
+  double x = F_hyper(0.5, 1.0, 1.5, -z * z);
+  //printf("Hypergeometriz = %20.17f\n", x);
+
+  double y = (fabs(z) < DBL_EPSILON) ? 1.0 : atan(z) / z;
+  //printf("Exact = %20.17f\n", y);
+
+  printf("%-16s = %20.17f\n", "Hypergeometriс", x);
+  printf("%-16s = %20.17f\n", "Exact", y);
+  // double eps = fabs((x - y)/y);
+  // printf("Eps = %9.1e\n", eps);
+  // tgamma(10); rint(); pow(2,3)
 }
